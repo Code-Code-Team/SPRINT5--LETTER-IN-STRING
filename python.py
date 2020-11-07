267565c672ffeeeaeedb3bb9bc130104c6d4cfc1
E2029-Aydin <aydinsanoz2@gmail.com>
2020-11-08 00:47:45 +0300

    python func added

---
+++ python.py
@@ -0,0 +1,5 @@
+
+string = input('Enter A String  : ')
+letter = input('Enter A Letter  : ')
+count = string.lower().count(letter.lower())
+print(f'{string} kelimesinin içerisinde {letter} kelimesi {count} defa geçmektedir')
